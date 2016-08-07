dc55b949066411bf285b7801cf611c1f93beb29c
danidee10 <osaetindaniel@gmail.com>
2016-08-07 14:02:53 +0100

    added protection for <link /> tags that are urls

--- staticfy.py
+++ staticfy.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 from bs4 import BeautifulSoup
 import re
 import os
@@ -9,11 +11,9 @@ parser.add_argument('--static-endpoint', help='static endpoint which is usually 
 parser.add_argument('--template-folder', help='template folder that contains the html file(s)')
 args = parser.parse_args()
 
-
 results = []  # list that holds the links, images and scripts as they're found by BeautifulSoup
-static_url = args.static_endpoint
-static_url = static_url if static_url else 'static' # fall back to the default static if no endpoint is specified
-template_folder = args.template_folder
+static_url = args.static_endpoint or 'static'  # fall back to the default static if no endpoint is specified
+template_folder = args.template_folder or ''  # set template folder to an empty string if it wasn't provided
 
 filename = args.filename
 in_file = os.path.join(template_folder, filename)
@@ -24,8 +24,8 @@ html_doc = BeautifulSoup(file_handle, 'html.parser')
 # find all images in the file
 image_links = html_doc.find_all('img', src=True)
 
-# protect against externally links like Google fonts, FontAwesome etc
-links = html_doc.find_all(lambda x: True if x.name == 'link' and 'http' not in x.get('href') else False)
+# protect against external links like Google fonts, FontAwesome etc
+links = html_doc.find_all(lambda x: True if x.name == 'link' and not x.get('href').startswith(('http', '//')) else False)
 
 # find all script tags
 scripts = html_doc.find_all('script', src=True)
@@ -47,7 +47,6 @@ file_handle.close()
 # create the staticfy and the appropriate template folder
 template_folder = template_folder.split(os.path.sep)[-1]
 out_file = os.path.join('staticfy', template_folder, filename)
-print(os.path.dirname(out_file))
 os.makedirs(os.path.dirname(out_file), exist_ok=True)
 
 with open(in_file, 'r') as input_file, open(out_file, 'w') as output_file:
@@ -60,7 +59,7 @@ with open(in_file, 'r') as input_file, open(out_file, 'w') as output_file:
                 output_file.write(file_line)
                 break
         else:
-            print(file_line)
+            # print(file_line)
             output_file.write(file_line)
 
     print('staticfied \033[94m{} ==> \033[92m{}\n'.format(in_file, out_file))
