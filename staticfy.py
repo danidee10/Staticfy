782ebb0dc746c199ccd2841474f9f3e53d72dc4d
Osaetin Daniel <osaetindaniel@gmail.com>
2016-09-11 15:07:58 +0100

    Added support to staticfy all html files in a directory if 'template-folder' is the only argument that is specified. Fixed an issue with the terminal colour (on bash) not resetting back to the default colour, after printing out the staticfy success message(s). Added exception hanlding to prevent the program from crashing on itself when the file to staticfy isn't found.

--- staticfy.py
+++ staticfy.py
@@ -1,29 +1,43 @@
 #! /usr/bin/python3
 from bs4 import BeautifulSoup
+import sys
 import re
 import os
 import argparse
 
 def parse_cmd_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', type=str, help='Filename of the file to be staticfied')
+    parser.add_argument('filename', type=str, nargs='?', help='Filename of the file to be staticfied')
     parser.add_argument('--static-endpoint', help='static endpoint which is usually == "static"')
     parser.add_argument('--template-folder', help='template folder that contains the html file(s)')
     args = parser.parse_args()
 
     filename, static_endpoint, template_folder = args.filename, args.static_endpoint, args.template_folder
-    staticfy(filename, static_endpoint=static_endpoint, template_folder=template_folder)
+
+    if not filename and template_folder:
+        # get all the files in the folder and staticfy them
+        for file in os.scandir(template_folder):
+            if file.name.endswith(('htm', 'html')) and file.is_file():
+                staticfy(file.name, static_endpoint=static_endpoint, template_folder=template_folder)
+    else:
+        staticfy(filename, static_endpoint=static_endpoint, template_folder=template_folder)
 
 
 
 def staticfy(filename, static_endpoint='static', template_folder=''):
 
     results = []  # list that holds the links, images and scripts as they're found by BeautifulSoup
-    static_url = static_endpoint or 'static'  # fall back to the default static if no endpoint is specified
-    template_folder = template_folder or ''  # set template folder to an empty string if it wasn't provided
+    static_url = static_endpoint or 'static' # incase None is passed as the static_endpoint
+    template_folder = template_folder or '' # incase None is passed as the template_folder
 
     in_file = os.path.join(template_folder, filename)
-    file_handle = open(in_file)
+
+    try:
+        file_handle = open(in_file)
+    except FileNotFoundError as e:
+        print('\033[91m' + 'File not found' + '\033[0m')
+        sys.exit(1)
+
     html_doc = BeautifulSoup(file_handle, 'html.parser')
 
     # find all images in the file
@@ -70,13 +84,9 @@ def staticfy(filename, static_endpoint='static', template_folder=''):
                 # print(file_line) --verbose option
                 output_file.write(file_line)
 
-        print('staticfied \033[94m{} ==> \033[92m{}\n'.format(in_file, out_file))
+        print('staticfied \033[94m{} ==> \033[92m{}\033[0m\n'.format(in_file, out_file))
 
         return out_file
-
-        # reset the terminal color back to it's normal color
-        # no need for this in zsh
-        # os.system('cls||reset') should work for bash  # dunno about windows ¯\_(ツ)_/¯
 
 if __name__ == '__main__':
     parse_cmd_arguments()
