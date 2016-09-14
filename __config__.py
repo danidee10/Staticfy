eb13e0e66ac07b0d53d0b90f9f411a4492560ae5
Sama <gbozee@gmail.com>
2016-09-14 19:49:02 +0100

    renamed variables and ensured pep8 style

--- __config__.py
+++ __config__.py
@@ -1,9 +1,6 @@
 
 frameworks = {
-    'flask': {
-        'format': "{{ url_for('%(endpoint)s', filename='%(attr_name)s') }}"
-    },
-    'django': {
-        'format': "{%% static '%(attr_name)s' %%}"
-    }
+    'flask':  "{{ url_for('%(endpoint)s', filename='%(attr_name)s') }}"
+    ,
+    'django':"{%% static '%(attr_name)s' %%}"
 }
