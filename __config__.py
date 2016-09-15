f348903ea4c2afad2b40ee0303ac7e6e7cfcd345
Sama <gbozee@gmail.com>
2016-09-15 10:26:50 +0100

    added

--- __config__.py
+++ __config__.py
@@ -1,6 +1,5 @@
 
 frameworks = {
-    'flask':  "{{ url_for('%(endpoint)s', filename='%(attr_name)s') }}"
-    ,
+    'flask':  "{{ url_for('%(endpoint)s', filename='%(attr_name)s') }}",
     'django':"{%% static '%(attr_name)s' %%}"
 }
