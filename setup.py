5ceaa0ef40440a3168d7fd7ee260125205ed8901
Osaetin Daniel <osaetindaniel@gmail.com>
2016-09-21 05:13:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(name='Staticfy',
       version='0.1',
       description='Convert static assets links to dynamic web framework links',
-      url='https://github.com/danidee10/funniest',
+      url='https://github.com/danidee10/Staticfy',
       author='Osaetin Daniel',
       author_email='osaetindaniel@gmail.com',
       license='GPL',
