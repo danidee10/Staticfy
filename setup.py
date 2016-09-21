ebb94fb8688b351bd9a0abf2f5e6b1dade66097a
Osaetin Daniel <osaetindaniel@gmail.com>
2016-09-21 04:48:55 +0100

    Added setup.py for easy installation with pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Staticfy',
+      version='1.0',
+      description='Convert static assets links to dynamic web framework links',
+      author='Osaetin Daniel',
+      author_email='osaetindaniel@gmail.com',
+      url='https://danidee10.github.io',
+      packages=['bs4'],
+     )
