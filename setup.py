a31575da3cac50667240d5c5bb34e1525f5befba
Osaetin Daniel <osaetindaniel@gmail.com>
2018-02-09 11:00:04 +0100

    2.0 Release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='Staticfy',
-      version='1.9',
+      version='2.0',
       description='Convert static assets links to dynamic web framework links',
       url='https://github.com/danidee10/Staticfy',
       author='Osaetin Daniel',
